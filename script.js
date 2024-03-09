d0e7d2d356e6c8208a92c3633243b8367368d4b3
Shivam-0804 <shivamchaudhary0408@gmail.com>
2024-03-10 00:52:22 +0530

    changes

--- script.js
+++ script.js
@@ -1,11 +1,22 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const input = document.getElementById("current");
-    const output =document.getElementById("destination");
-    const options = {
-        types: ["geocode"],
-        componentRestrictions: { country: "ind" } 
-    };
+var origin, destination;
 
-    const origin = new google.maps.places.Autocomplete(input, options);
-    const destination =new google.maps.places.Autocomplete(output, options);
+document.addEventListener("DOMContentLoaded", function () {
+  // Date updating
+  const currentDate = new Date();
+  const year = currentDate.getFullYear();
+  const month = String(currentDate.getMonth() + 1).padStart(2, "0");
+  const day = String(currentDate.getDate()).padStart(2, "0");
+  const formattedDate = `${year}-${month}-${day}`;
+  document.getElementById("date").value = formattedDate;
+
+  //input options
+  const input = document.getElementById("current");
+  const output = document.getElementById("destination");
+  const options = {
+    types: ["geocode"],
+    componentRestrictions: { country: "ind" },
+  };
+
+  origin = new google.maps.places.Autocomplete(input, options);
+  destination = new google.maps.places.Autocomplete(output, options);
 });
